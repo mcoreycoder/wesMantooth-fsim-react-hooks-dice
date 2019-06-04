6187b66379f5f4a80b360cfa8b25eb8e658d5a94
Michael Corey <fishincorey@gmail.com>
2019-06-04 11:18:50 -0600

    refactored a bit to remove the newDice and newHistory variables and simplify the update of state that they were used for

--- src/DiceBag.js
+++ src/DiceBag.js
@@ -4,7 +4,6 @@ const Die = ({ numSides }) => {
     const [roll, setRoll] = useState(0) // [roll is state that holds a single value, setRoll is the function to update the roll state value] = useState(defaultState) is a function to set up the hook and set default value for this hooks state 
     const [history, setHistory] = useState([]) // [history is state which is an array to hold a collection, setHistory is a function to update the history state collection] = useState(defaultState) is a function to set up the hook and set default value for this hooks state  
     // console.log('sides', numSides, '| roll', roll, '| history', history) // used for checking data flow
-    let newHistory = history // variable used for adding the roll state to before updating the history state
        
     return (
         <div className='box'>
@@ -13,10 +12,9 @@ const Die = ({ numSides }) => {
             </pre>
             {history.join(', ')} {/** takes the histroy collection and displays as string with comma seperating each item of the array */}
             <button onClick={() => { setRoll(Math.ceil(Math.random() * numSides));  if(roll !== 0 ){
-        // console.log('newHistory', newHistory) // used to check data
         // console.log('history', history) // used to check data
-        newHistory= [...newHistory,roll] // updates newHistory by taking in the spread of the current newHistory array and adding the roll state value to it.
-    }; setHistory(newHistory) }}> {/** updates the history state */}
+        setHistory([...history,roll]) // updates state history by taking in the spread of the current state history array and adding the roll state value to it.
+    };  }}> 
                 Roll
             </button>
         </div>
@@ -45,11 +43,10 @@ const defaultDice = [
 const DiceBag = () => {
     const [dice, setDice] = useState(defaultDice) // const [dice is state which is an array to hold a collection, setDice is a function to update the state of dice] = useState(defaultState) is a function to set up the hook and set default value for this hooks state 
     const [input, setInput] = useState(0) // [input is state which is a number to hold a single value, setInput is a function to update the state of input] = useState(defaultState) is a function to set up the hook and set default value for this hooks state 
-    const newDice = dice // takes in the current state of dice
     return (
         <div>
             <input type="number" onChange={(event) => setInput(event.target.value)} /> {/** input state is updated with onChange event*/ }
-            <button onClick={() => setDice([...newDice,{numSides:input}])}>Add Die</button> {/** takes the input and adds it to the newDice array then updates the state for dice */}
+            <button onClick={() => setDice([...dice,{numSides:input}])}>Add Die</button> {/** sets the dice state by spreading the current state dice array and adding the current state input to the end of the array*/}
             <Dice dice={dice}/> {/** <Dice> takes in the state "={dice}" to pass as a prop with the name of dice, example: nameOfProp={state} */}
         </div>
     )
